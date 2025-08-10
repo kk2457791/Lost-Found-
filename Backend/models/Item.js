0e2a0269608c52f6edfefd777d20048d432073a8
kk2457791 <kumaryadavkiran84@gmail.com>
2025-08-10 16:48:31 +0530

    Update Item.js

--- Backend/models/Item.js
+++ Backend/models/Item.js
@@ -1,13 +1,1 @@
-/*import mongoose from 'mongoose';
 
-const ItemSchema = new mongoose.Schema({
-  type: { type: String, enum: ['lost', 'found'], required: true },
-  description: String,
-  imageUrl: String,
-  location: { lat: Number, lng: Number },
-  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-  createdAt: { type: Date, default: Date.now }
-});
-
-export default mongoose.model('Item', ItemSchema);
-  */
